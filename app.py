33f3e8df6b0ee145b5b71b83dadc414ff7f35527
Nael Ebrahim <naelebrahim926@gmail.com>
2025-06-23 00:25:13 +0300

    add: Add basic front end application

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
-from ir_project.search_engine import SearchEngine
+# from search_engine import SearchEngine
 
 app = Flask(__name__)
 
 # Initialize the search engine once
-search_engine = SearchEngine()
+# search_engine = SearchEngine()
 
 @app.route('/')
 def index():
@@ -18,8 +18,21 @@ def search():
     if not query:
         return render_template('index.html', error="Please enter a search query.")
     
-    results = search_engine.search(query)
-    
+    # results = search_engine.search(query)
+    results = [
+        {
+            'result_id': 1,
+            'title': 'Document 1',
+            'snippet': 'This is the first document.',
+            'result_detail': 'sfsdf'
+        },
+        {
+            'result_id': 2,
+            'title': 'Document 2',
+            'snippet': 'This is the second document.',
+            'result_detail': 'sfsdf'
+        }
+    ]
     return render_template('results.html', query=query, results=results)
 
 if __name__ == '__main__':
