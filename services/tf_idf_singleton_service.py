a2de16a53416e47042db0cbb3f93697b8787c4d6
Nael Ebrahim <naelebrahim926@gmail.com>
2025-07-05 20:18:50 +0300

    fix(tfidf-search): Fix search method to return document count
    The search method was returning an incorrect value, now the method
    returns the document count and the search results.

--- services/tf_idf_singleton_service.py
+++ services/tf_idf_singleton_service.py
@@ -29,7 +29,7 @@ class TFIDFSingletonService:
         for dataset_name in self.available_datasets:
             self.tf_idf_services[dataset_name] = TFIDFService()
             self.tf_idf_services[dataset_name].load_model(dataset_name)
-        
+
         self.index_service = InvertedIndexSingletonService()
         self._initialized = True
 
@@ -44,9 +44,9 @@ class TFIDFSingletonService:
         for token in processed_tokens:
             if token in self.index_service.inverted_indices[dataset_name]:
                 candidate_indices.update(self.index_service.inverted_indices[dataset_name][token])
-        
+
         candidate_indices = list(candidate_indices)
-        
+
         vectorizer = self.get_tfidf_service(dataset_name).get_vectorizer()
         tfidf_matrix = self.get_tfidf_service(dataset_name).get_tfidf_matrix()
 
@@ -59,10 +59,10 @@ class TFIDFSingletonService:
 
         if len(candidate_indices) == 0:
             print("No matching documents found for the query")
-            return []
-        
+            return 0, []
+
         candidate_doc_vectors = tfidf_matrix[candidate_indices]
-        
+
         try:
             cosine_similarities = cosine_similarity(query_vector, candidate_doc_vectors).flatten()
             return self._get_top_results(cosine_similarities, top_n, dataset_name, candidate_indices)
@@ -79,10 +79,10 @@ class TFIDFSingletonService:
                 doc_id = self.index_service.index_to_doc_ids[dataset_name].get(original_index)
                 if doc_id:
                     results.append({"doc_id": doc_id, "score": cosine_similarities[i] })
-        
+
         # Convert top_n to integer to ensure proper slicing
         top_n = int(top_n) if top_n != -1 else -1
-        
+
         if top_n == -1:
             return len(results), sorted(results, key=lambda x: x["score"], reverse=True)
         return len(results), sorted(results, key=lambda x: x["score"], reverse=True)[:top_n]
@@ -97,4 +97,3 @@ if __name__ == "__main__":
     print('In dataset: ', dataset_name)
 
     print(tf_idf_service.search(query, dataset_name, top_n))
-    
