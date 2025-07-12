4af2e2fce50b4d2e503883a3cc752b0173880885
Nael Ebrahim <naelebrahim926@gmail.com>
2025-07-12 09:52:06 +0300

    update: Delete database config file

--- config.py
+++
@@ -1,8 +0,0 @@
-# Database configuration
-# Please replace the values below with your actual MySQL credentials.
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',  # Replace with your MySQL username
-    'password': '',  # Replace with your MySQL password
-    'database': 'ir_project_db' # The name of the database to be created
-}
